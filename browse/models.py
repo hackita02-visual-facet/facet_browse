999523c172e3a09fc410c75a20fdc4456e15c620
Amit Ziv-Kenet <amit.zivkenet@gmail.com>
2015-11-18 19:00:03 +0200

    kinda works, very sucks

--- browse/models.py
+++ browse/models.py
@@ -1,7 +1,29 @@
+import json
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.urlresolvers import reverse
 from django.db import models
 
-# Create your models here.
+from facet_core import primo
 
 class FacetQuery(models.Model):
-    query = models.TextField(unique=True)
+    query = models.TextField()
     facets = models.TextField()
+
+
+    def get_facets(self):
+        facets = primo.facet_query(self.query)
+        self.facets = json.dumps(facets)
+
+    def get_absolute_url(self):
+        return reverse("query", args=(self.pk,))
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        try:
+            o =  FacetQuery.objects.get(query=self.query)
+            self.pk = o.pk
+            self.facets = o.facets
+        except ObjectDoesNotExist:
+            self.get_facets()
+            super().save(force_insert, force_update, using, update_fields)
+
--- browse/views.py
+++ browse/views.py
@@ -1,36 +1,24 @@
-from django.shortcuts import render
-import json
+from django import forms
 from django.views.generic import CreateView, DetailView
-from django.core.urlresolvers import reverse_lazy
 
 from . import models
 
-from facet_core import primo
-
-# Create your views here.
-
-class Query(CreateView,DetailView):
-    model = models.FacetQuery
-
-    fields = (
-        'query',
-    )
-
-    success_url = reverse_lazy("query",args=[object.pk])
-
-    def form_valid(self,form):
-        qs = super().get_queryset().filter(
-            query = form.instance.query
+class QueryForm(forms.ModelForm):
+    class Meta:
+        model = models.FacetQuery
+        fields = (
+            'query',
         )
 
-        if qs:
-            form.instance.facets = qs[0].facets
-        else:
-            fcs = primo.facet_query(
-                form.instance.query
-            )
-
-            form.instance.facets = json.dumps(fcs)
+class QueryCreateView(CreateView):
+    model = models.FacetQuery
+    form_class = QueryForm
 
 
+class QueryDetailView(DetailView):
+    model = models.FacetQuery
 
+    def form(self):
+        return QueryForm(initial={
+            'query': self.get_object().query
+        })

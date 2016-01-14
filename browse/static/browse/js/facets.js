6eeedc08370d68ad34ecb461209d7545caa4ae0c
Amit Ziv-Kenet <amit.zivkenet@gmail.com>
2016-01-14 15:00:18 +0200

    shitload of work

---
+++ browse/static/browse/js/facets.js
@@ -0,0 +1,24 @@
+/**
+ * Created by azk on 14/01/16.
+ */
+
+var facet_pretty_names = {
+    local5: "דובר/מבצע",
+    local2: "הרכב מוסיקלי",
+    rtype: "סוג חומר",
+    genre: "סוגה",
+    creator: "מחבר/יוצר",
+    creationdate: "שנת פרסום",
+    local1: "אולם קריאה",
+    lang: "שפה",
+    domain: "אוסף",
+    topic: "נושאים",
+
+    pfilter: "pfilter",
+    library: "library",
+    local7: "local7",
+    local3: "local3",
+    lcc: "lcc",
+    tlevel: "tlevel",
+    local6: "local6"
+}
